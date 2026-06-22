4c49030befac13467076740b27b47cdb4323d173
Ishanth-777 <ishanthnmki777@gmail.com>
2026-06-22 16:52:32 +0530

    Create employee model and routes

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,24 @@ const express = require("express");
 const cors = require("cors");
 
 const connectDB = require("./config/db");
-
+const employeeRoutes =
+require("./routes/employeeRoutes");
 const app = express();
 
 connectDB();
 
 app.use(cors());
+app.use("/api/employees", employeeRoutes);
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("ERP Backend Running");
 });
+
+app.use(
+"/api/employees",
+employeeRoutes
+);
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
