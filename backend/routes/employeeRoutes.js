1fd135e2121420195cf73fa81992bed9544725e8
Ishanth-777 <ishanthnmki777@gmail.com>
2026-06-23 15:06:32 +0530

    Connect employee API with MongoDB

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -2,15 +2,75 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Employee API Working");
+const Employee =
+require("../models/Employee");
+
+router.get(
+"/",
+async(req,res)=>{
+
+try{
+
+const employees =
+await Employee.find();
+
+res.json(
+employees
+);
+
+}
+
+catch(error){
+
+res.status(500)
+.json({
+message:
+error.message
 });
 
-router.post("/add", (req, res) => {
-  res.json({
-    message: "Employee Added",
-    data: req.body
-  });
+}
+
 });
 
-module.exports = router;
+router.post(
+"/add",
+async(req,res)=>{
+
+try{
+
+const employee =
+new Employee(
+req.body
+);
+
+await employee.save();
+
+res.status(201)
+.json({
+
+message:
+"Employee Added",
+
+data:
+employee
+
+});
+
+}
+
+catch(error){
+
+res.status(500)
+.json({
+
+message:
+error.message
+
+});
+
+}
+
+});
+
+module.exports =
+router;
